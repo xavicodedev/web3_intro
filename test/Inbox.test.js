f3dadc2ce431be8c8203709be99425efad8ff603
Xavi <you@example.com>
2022-01-14 14:34:18 +0100

    Created first test to deploy contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,4 +1,29 @@
+const { equal } = require('assert');
 const assert = require('assert');
+const { Console } = require('console');
 const ganache = require('ganache-cli');
-const Web3 = require('Web3');
-const web3 = new Web3(ganache.provider());
+const { before } = require('mocha');
+const { beforeEach } = require('mocha');
+const { linkBytecode } = require('solc');
+//Web3 Constructor
+const Web3 = require('Web3'); 
+//Web3 Instance 
+const web3 = new Web3(ganache.provider()); 
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+    //Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    //Use one of the account to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hola que tal']  })
+    .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts);
+    });
+});
